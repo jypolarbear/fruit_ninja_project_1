7414ea8af3535b7201d64925236d5230c394315f
James Yun <jamesyun@ool-ad0205ef.dyn.optonline.net>
2019-02-08 09:24:43 -0500

    implemented game logics, and start the game, styled fonts, got scoreboard and timer.

--- README.md
+++ README.md
@@ -3,10 +3,9 @@
 
 ## Game Description:
 
-The game is called Fruit Shooter similar to the game Fruit Ninja.
-The user needs to shoot as many bananas as they can until the time runs out. There will be
-bombs as well, the user needs to avoid the bombs and only shoot the bananas. If the user certain amount of bombs (3) the game resets and you lose.
-If not, the user wins with the amount of bananas they shot down. The score boards shows how many bananas you shot and how many bombs you shot.
+The game is called Banana Ninja similar to the game Fruit Ninja.
+The user needs to destroy as many bananas as they can until the time runs out.
+The score board tracks the amount of bananas you destroyed.
 
 ## Main technology
 
@@ -15,10 +14,29 @@ CSS
 Javascript
 
 ## Major problems
+Getting infinite amount of bananas into a array and have it set inside set interval.
 Making the game responsive to different view port sizes. Fruits will be in different sizes or certain width.
 
 
 ## Post MVP
 
-Have the the option to choose weapons. The weapon will be a sword, with this sword instead of having the shooting effect, you will be able to slice the Fruits
-(bananas).
+Have the the option to choose weapons. The weapon will be a sword and a gun, with this sword the user will get an animation as they destroy bananas. The gun will give animation effect as well. There will be bombs as well, the user needs to avoid the bombs and only shoot the bananas. If the user shoots certain amount of bombs (3) the game resets and you lose.
+
+# Code Snippet
+```
+
+const bananasArr = [];
+
+const createBanana = () => {
+  bananasArr.push({ position: Math.floor(Math.random()), node: document.createElement('div') });
+  bananasArr[bananasArr.length - 1].node.classList.add('bananas');
+  bananasArr[bananasArr.length - 1].node.style.left = Math.random() * window.innerWidth + 'px';
+  const grabDivArena = document.querySelector('#arena');
+  grabDivArena.appendChild(bananasArr[bananasArr.length - 1].node);
+
+  const numberBananasCreated = document.querySelectorAll('.bananas').length
+  const bananaCreated = document.querySelectorAll('.bananas')[numberBananasCreated - 1];
+};
+
+```
+This block of codes creates the element div which is assigned an attribute class called 'bananas'. This element is then pushed inside an empty array as an object with the properties, position and node. The node creates the element and position gives the element random width positions in the document window. Every time this function is called it takes out the last array and sends it into the DOM. 
--- js/script.js
+++ js/script.js
@@ -7,95 +7,161 @@ const grabButton = document.querySelector('#button');
 // MAIN INTRO DIV CLASS THAT HOLDS ELEMENT H2
 const grabMainIntro = document.querySelector('.main-intro');
 
+//Grab main header class
+const grabMainHeader = document.querySelector('.main-header');
+
 // body
 const body = document.querySelector('body');
 
 
+//Get score
+let userScore = 0;
+let grabScore;
+
+//Set / Get timer
+
+let userTimer = 30;
+let grabTime;
+
+//bananas Array
+const bananasArr = [];
+let arena;
+let movingBanana;
+let gameTimer;
 
 
 
   //THIS FUNCTION WILL REMOVE ONLY WELCOME INTRO
-function clickKapow() {
+const clickKapow = () => {
   createNewInstruction();
   removeWelcomeIntro();
   newButton();
-
 }
+
+
 grabButton.addEventListener('click', clickKapow);
 
-function removeWelcomeIntro() {
+const removeWelcomeIntro = () => {
   const welcomeIntro = document.querySelector('#welcome'); //THIS IS THE FUNCTION THAT CLICKKAPOW WILL CALL.
   welcomeIntro.remove();
   grabButton.remove();
-  grabButton.removeEventListener('click', clickKapow);
 }
 
 
-function createNewInstruction() {
+
+const createNewInstruction = () => {
   const newInstructionIntro = document.createElement('h2');
   newInstructionIntro.setAttribute('id', 'instruction');
   newInstructionIntro.innerHTML = 'Welcome Great Shinobi';
   grabMainIntro.appendChild(newInstructionIntro);
   const newInstructionDes = document.createElement('p');
   newInstructionDes.setAttribute('id', 'instruction-desc');
-  newInstructionDes.innerHTML = `<p>You are the next great ninja shooter. Destroy as many bananas as you can within the time limit given to you to become the master ninja. <br> Watchout for those bombs! Hit KaPOW to start</p>`
+  newInstructionDes.innerHTML = `<p>You are the next great ninja. Destroy as many bananas as you can within the border and time limit given to you to become the master ninja. <br> Hit KaPOW to start!!</p>`
   grabMainIntro.appendChild(newInstructionDes);
 }
 
 
 
-function newButton() {
+
+
+const newButton = () => {
   const createNewButton = document.createElement('button');
   createNewButton.setAttribute('id', 'button');
   grabMainInputUser.appendChild(createNewButton);
   createNewButton.innerHTML = 'KA POW!!!'
   console.log('new button added');
-  createNewButton.addEventListener('click', newButton);
+  createNewButton.addEventListener('click', changeThisName);
 }
-//
-// function prepareGame() {
-//   const intro = document.querySelector('#instruction' 'instruction-desc'); //THIS IS THE FUNCTION THAT CLICKKAPOW WILL CALL.
-//   intro.remove();
-//   createNewButton.remove();
-//   createNewButton.removeEventListener('click', newButton);
-// }
+
+const changeThisName = () => {
+  let variableName = document.querySelector('#button');
+  variableName.remove();
+  const newInstructionIntro = document.querySelector('#instruction');
+  const newInstructionDes = document.querySelector('p');
+  newInstructionIntro.remove();
+  newInstructionDes.remove();
+  createScoreBoard();
+  score();
+  movingBanana = setInterval(movingFrame, 90); //RUN THIS TO HAVE BANANAS, TO START THE ACTUAL GAME.
+  gameTimer = setInterval(setTimer, 1000);
+}
+
+const setTimer = () => {
+  grabTime = document.querySelector('#time-set');
+  userTimer--;
+  grabTime.innerHTML = `${userTimer}`;
+  if (userTimer === 0) {
+    clearInterval(gameTimer);
+    alert(`Game Over!! Your score is ${userScore}!!`);
+    clearInterval(movingBanana);
+  }
+
+}
+
+const createScoreBoard = () => {
+  const getScore = document.createElement('div');
+  getScore.setAttribute('id', 'realScoreTimer');
+  getScore.innerHTML = `<h2>Timer</h2> <h2>Score Board</h2>`;
+  grabMainIntro.appendChild(getScore);
+}
+
+const score = () => {
+  const score = document.createElement('div');
+  score.setAttribute('id', 'score');
+  score.innerHTML = `<p id ='time-set'>30</p> <p id ='actualScores'>0</p>`;
+  grabMainIntro.appendChild(score);
+  grabScore = document.querySelector('#actualScores');
+}
 
 
-// const newfooter = document.createElement('footer');
-// newfooter.setAttribute('id', 'game-arena');
-// document.body.appendChild(newfooter);
 
 
-// prepareGame();
 
 
-const createArena_Banana = () => {
+const createArena = () => {
   const createArenaDiv = document.createElement('div');
   createArenaDiv.setAttribute('id', 'arena');
   document.body.append(createArenaDiv);
-
-  const createAnimatedBanana = document.createElement('div');
-  createAnimatedBanana.setAttribute('id', 'bananas');
-  createArenaDiv.appendChild(createAnimatedBanana);
+  arena = createArenaDiv;
 }
 
-createArena_Banana();
+createArena();
 
-
-
-function movingBananaFunction() {
-let position = 0;
-const movingBanana = setInterval(frame, 2);
-
-function frame(banana) {
-  const grabBanana = document.querySelector('#bananas');
-  if (position === 350) {
-    clearInterval();
-  } else {
-    position++;
-    grabBanana.style.bottom = position + 'px';
-    // grabBanana.style.right = position + 'px';
+const bananasPerish = (event) => {
+  if (event.target.className === "bananas") {
+    event.target.remove();
+    userScore++;
+    grabScore.innerHTML = `${userScore}`;
   }
 }
+
+arena.addEventListener("mouseover", bananasPerish)
+
+const createBanana = () => {
+  bananasArr.push({ position: Math.floor(Math.random()), node: document.createElement('div') });
+  bananasArr[bananasArr.length - 1].node.classList.add('bananas');
+  bananasArr[bananasArr.length - 1].node.style.left = Math.random() * window.innerWidth + 'px';
+  const grabDivArena = document.querySelector('#arena');
+  grabDivArena.appendChild(bananasArr[bananasArr.length - 1].node);
+
+  const numberBananasCreated = document.querySelectorAll('.bananas').length
+  const bananaCreated = document.querySelectorAll('.bananas')[numberBananasCreated - 1];
+};
+
+
+
+const movingFrame = () => {
+  bananasArr.forEach(banana => {
+    if (banana.position > 540) {
+      banana.node.remove();
+    }
+    banana.position += 10;
+
+    //changing each banana position
+    banana.node.style.bottom = banana.position + 'px';
+  })
+  bananasArr.filter(banana => {
+    return banana.position < 540;
+  })
+  createBanana();
 }
-movingBananaFunction();
